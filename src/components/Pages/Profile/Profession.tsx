de8012ea1ee1f063d4f24f46f725f64e391ae71a
Muhamad Aqil Maulana <muhamadaqil383@gmail.com>
2024-08-14 03:06:44 +0700

    update

--- src/components/Pages/Profile/Profession.tsx
+++ src/components/Pages/Profile/Profession.tsx
@@ -1,9 +1,11 @@
 import FullStackDeveloperProfession from "./FSD";
+import WriterProfession from "./WR"
 import { useProfileData } from "./ProfileProvider";
 
 export default function Profession() {
   const { field } = useProfileData();
   if (field === "fullstack-developer") return <FullStackDeveloperProfession />;
+  else if(field === "writer") return <WriterProfession />
 
   return <div>Anda belum memilih profesi yang ingin dilihat</div>;
 }
--- src/components/Pages/Profile/WR/index.tsx
+++ src/components/Pages/Profile/WR/index.tsx
@@ -2,6 +2,11 @@ import React from "react";
 
 export default function WriterProfession(){
     return(
-       <div></div> 
+       <div>
+        <div className="grid grid-cols-[30%_auto] p-4 min-h-screen">
+            <p>Gambar</p>
+            <p>Ulasan</p>
+        </div>
+       </div> 
     )
 }
