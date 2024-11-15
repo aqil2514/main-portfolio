7c11f2f22808c537bc6cc3fc1001fdad2368e384
Muhamad Aqil Maulana <muhamadaqil383@gmail.com>
2024-11-16 03:12:10 +0700

    remove certificate|

--- src/components/Pages/Profile/WHA/index.tsx
+++ src/components/Pages/Profile/WHA/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Greetings from "./Greetings";
-import { AnimatedIcons, Certificates } from "../_components";
-import { certificates, icons } from "./data";
+import { AnimatedIcons } from "../_components";
+import { icons } from "./data";
 import { useProfileData } from "../ProfileProvider";
 import ProfessionContainer from "@/components/layout/Containers/Profession";
 
@@ -14,9 +14,9 @@ export default function WareHouseAdminProfession() {
         <AnimatedIcons icons={icons} />
         <Greetings />
       </ProfessionContainer>
-      <div className="min-h-screen py-4" ref={certifRef}>
+      {/* <div className="min-h-screen py-4" ref={certifRef}>
         <Certificates certificates={certificates} />
-      </div>
+      </div> */}
     </div>
   );
 }
