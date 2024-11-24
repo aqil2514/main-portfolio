a464469f456fc5668b5f53a1336082f058e69b6a
Muhamad Aqil Maulana <muhamadaqil383@gmail.com>
2024-11-25 02:30:30 +0700

    work expereince

--- src/components/Pages/Profile/WHA/WorkExperience.tsx
+++ src/components/Pages/Profile/WHA/WorkExperience.tsx
@@ -7,14 +7,14 @@ import React from "react";
 export default function WorkExperience() {
   return (
     <WorkExperienceContainer>
-        <h2 className="font-young-serif my-4">Pengalaman Kerja</h2>
+      <h2 className="font-young-serif my-4">Pengalaman Kerja</h2>
       {workExperience.map((work) => (
         <div
           key={work.companyName}
           className="flex flex-col gap-4 bg-zinc-800 md:max-w-[580px] rounded-xl p-4"
         >
           {/* Gambar dan detail singkat */}
-          <div className="flex gap-4">
+          <div className="flex flex-col md:flex-row gap-4 justify-center items-center">
             {/* Gambar logo perusahaan */}
             <div className="relative w-16 h-16 rounded-full overflow-hidden">
               <Image
@@ -26,9 +26,9 @@ export default function WorkExperience() {
             </div>
             {/* Nama perusahaan, posisi, deskripsi singkat, lokasi */}
             <div>
-              <p>
+              <p className="text-center md:text-left">
                 {work.companyName}{" "}
-                <span className="italic">
+                <span className="italic hidden md:inline">
                   ({work.duration.startDate} s/d{" "}
                   {work.duration.endDate === "present"
                     ? "Sekarang"
@@ -36,10 +36,20 @@ export default function WorkExperience() {
                   )
                 </span>{" "}
               </p>
-              <div className="flex gap-2">
+              {/* Mobile Version */}
+              <p className="block md:hidden text-center">
+                ({work.duration.startDate} s/d{" "}
+                {work.duration.endDate === "present"
+                  ? "Sekarang"
+                  : work.duration.endDate}
+                )
+              </p>
+              {/* Badge */}
+              <div className="flex justify-center gap-2 my-4">
                 <Badge>{work.position}</Badge>
                 <Badge>{work.location}</Badge>
               </div>
+              {/* Deskripsi pekerjaa */}
               <p>{work.description}</p>
             </div>
           </div>
@@ -54,13 +64,16 @@ export default function WorkExperience() {
   );
 }
 
-const WorkList: React.FC<{ list?: string[]; title:string }> = ({ list, title }) => {
+const WorkList: React.FC<{ list?: string[]; title: string }> = ({
+  list,
+  title,
+}) => {
   if (!list) return <></>;
 
   return (
     <section>
       <h4 className="font-mono">{title}</h4>
-      <ul className="grid grid-cols-2 pl-4">
+      <ul className="md:grid md:grid-cols-2 pl-4">
         {list.map((l) => (
           <li className="text-sm list-disc font-poppins" key={l}>
             {l}
